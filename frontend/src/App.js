d7fc753da2e2c6f0eeeef8b79678e15154149fbd
Aditya <aditya.tekale99@gmail.com>
2025-03-04 19:24:52 -0800

    config: added routes related to restaurant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,27 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./components/Signup";
 import Navbar from "./components/Navbar";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./components/Dashboard";
+import RestaurantDashboard from "./components/RestaurantDashboard"; // ✅ Import restaurant dashboard
 import Login from "./components/Login";
+import RestaurantSignup from "./components/RestaurantSignup";
+import RestaurantLogin from "./components/RestaurantLogin";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/restaurant/signup" element={<RestaurantSignup />} />
+                <Route path="/restaurant/login" element={<RestaurantLogin />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/restaurant/dashboard" element={<RestaurantDashboard />} /> 
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
