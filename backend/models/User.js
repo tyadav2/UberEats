6bba659c4e35a6c208baf87f83de458d7bf5ff2f
Tanya <37634659+tyadav2@users.noreply.github.com>
2025-03-05 18:37:15 -0800

    Added timestamps

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,10 +2,11 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
 const User = sequelize.define("User", {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-  name: { type: DataTypes.STRING, allowNull: false },
-  email: { type: DataTypes.STRING, unique: true, allowNull: false },
-  password: { type: DataTypes.STRING, allowNull: false },
-}, { timestamps: false }); // Add this
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name: { type: DataTypes.STRING(100), allowNull: false },
+    email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+    password: { type: DataTypes.STRING(255), allowNull: false },
+    createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+}, { timestamps: false });
 
 module.exports = User;
