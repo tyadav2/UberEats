72f0671c3b032689b1f1460137602c6fb1a74c87
Aditya Tekale <aditya.tekale99@gmail.com>
2025-03-06 00:49:50 -0800

    config: added right entry points

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,21 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/restaurants", require("./routes/restaurantRoutes"));
-app.use("/api/orders", require("./routes/orderRoutes"));
+const userRoutes = require("./routes/userRoutes");
+const restaurantRoutes = require("./routes/restaurantRoutes");
+const dishRoutes = require("./routes/dishRoutes"); 
+const orderRoutes = require("./routes/orderRoutes");
+const favoriteRoutes = require("./routes/favoriteRoutes");
+
+app.use("/api/users", userRoutes);
+app.use("/api/restaurants", restaurantRoutes);
+app.use("/api/dishes", dishRoutes);  
+app.use("/api/orders", orderRoutes);
+app.use("/api/favorites", favoriteRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-
 
 const PORT = process.env.PORT || 5000;
 
