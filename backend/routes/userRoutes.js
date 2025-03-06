14400d3998f6ae4be28bd628a2a656bcb6d2f54b
Tanya <37634659+tyadav2@users.noreply.github.com>
2025-03-05 18:54:07 -0800

    using asynch for user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const { registerUser, loginUser, getUserProfile } = require("../controllers/userController");
 const { protect } = require("../middleware/authMiddleware");
+const User = require("../models/User");
 
 const router = express.Router();
 
 router.post("/signup", registerUser);
 router.post("/login", loginUser);
+
+
+
+// GET all users
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 
 // ✅ Fix: Use `getUserProfile` instead of `updateProfile`
 router.get("/profile", protect, getUserProfile); 
