2ba5fede25a8b1ab016a48d4ccf5950fa985b4b3
Tanya Yadav <tanyayadavv5@gmail.com>
2025-03-06 00:24:27 -0800

    model for favourites

---
+++ backend/models/Favorite.js
@@ -0,0 +1,32 @@
+// models/Favorite.js
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db"); 
+
+const Favorite = sequelize.define("Favorite", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Users", 
+      key: "id"
+    }
+  },
+  restaurantId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Restaurants", 
+      key: "id"
+    }
+  },
+}, {
+  
+  timestamps: true, 
+});
+
+module.exports = Favorite;
