70020ce96c94aeb1457439276d6f9c43e1476454
Tanya <aditya.tekale99@gmail.com>
2025-03-07 20:32:29 -0800

    feature: needs to be worked on yet

---
+++ frontend/src/pages/RestaurantDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RestaurantDashboard() {
+  return (
+    <div>
+      <h1>Restaurant Dashboard</h1>
+      <p>Welcome to your restaurant dashboard!</p>
+    </div>
+  );
+}
+
+export default RestaurantDashboard;
