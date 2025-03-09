1f4dac22d98891e196bc312a599f0f611f9b5863
Aditya Tekale <aditya.tekale99@gmail.com>
2025-03-08 23:30:57 -0800

    model: fav for restaurants on user end

--- backend/models/Favorite.js
+++ backend/models/Favorite.js
@@ -1,31 +1,44 @@
+'use strict';
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/db"); 
+const sequelize = require("../config/db");
+const Restaurant = require("./Restaurant");
+const User = require("./User");
 
-const Favorite = sequelize.define("Favorite", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "Users", 
-      key: "id"
+
+module.exports = (sequelize, DataTypes) => {
+  const Favorite = sequelize.define('Favorite', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
+    },
+    restaurantId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Restaurants',
+        key: 'id',
+      },
+      onDelete: 'CASCADE',
     }
-  },
-  restaurantId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "Restaurants", 
-      key: "id"
-    }
-  },
-}, {
+  }, {
+    timestamps: true
+  });
   
-  timestamps: true, 
-});
-
-module.exports = Favorite;
+  // Define associations
+  Favorite.associate = (models) => {
+    Favorite.belongsTo(models.User, { foreignKey: 'userId' });
+    Favorite.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' });
+  };
+  
+  return Favorite;
+};
