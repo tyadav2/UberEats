a3e68ec41531d0cdcd6fbc156ae5e7d40ea50121
Tanya <tanyayadavv5@gmail.com>
2025-03-09 20:51:23 -0700

    Fix: Fixed the order status values

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -10,8 +10,12 @@ const Order = sequelize.define("Order", {
     restaurantId: { type: DataTypes.INTEGER, allowNull: false, references: { model: Restaurant, key: "id" } },
     restaurantName: { type: DataTypes.STRING(100), allowNull: false },
     totalAmount: { type: DataTypes.FLOAT, allowNull: false },
-    status: { type: DataTypes.ENUM("Pending", "Preparing", "Delivered", "On the way", "Cancelled"), defaultValue: "Pending" },
+    status: { type: DataTypes.ENUM("Pick Up Ready", "Preparing", "Delivered", "On the way", "Cancelled", "New"), defaultValue: "New" },
     estimatedDeliveryTime: { type: DataTypes.STRING(20), allowNull: true },
+    deliveryAddress: {
+        type: DataTypes.STRING,
+        allowNull: true  // Allow null for pickup orders
+      },
     paymentMethod: { type: DataTypes.ENUM("Credit Card", "Cash", "Online"), allowNull: false },
     items: { type: DataTypes.JSON, allowNull: false },
     createdAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
